6eccdfdcca74db455d01413eb52d9e301875cf74
M-Kurek <kurekmaciek@gmail.com>
2017-10-06 19:57:31 +0200

    up to IP filter as example of custom filter usage

---
+++ src/main/java/packt/springboot/cookbook/WebConfiguration.java
@@ -0,0 +1,18 @@
+package packt.springboot.cookbook;
+
+import org.apache.catalina.filters.RemoteIpFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebConfiguration {
+    /**
+     * Out	of	the	box,	Spring	Boot	automatically	adds OrderedCharacterEncodingFilter	and	HiddenHttpMethodFilter
+     * this one is convenient Tomcat filter to translate IP behind the load balancer proxy
+     * @return
+     */
+    @Bean
+    public RemoteIpFilter remoteIpFilter() {
+        return new RemoteIpFilter();
+    }
+}
