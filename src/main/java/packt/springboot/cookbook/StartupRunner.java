9a9f1865c59391bdaec4e7beb6422db11d45506b
M-Kurek <kurekmaciek@gmail.com>
2017-10-06 19:26:59 +0200

    REST as usual

--- src/main/java/packt/springboot/cookbook/StartupRunner.java
+++ src/main/java/packt/springboot/cookbook/StartupRunner.java
@@ -4,6 +4,7 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.scheduling.annotation.Scheduled;
 import packt.springboot.cookbook.repository.BookRepository;
 
 import javax.sql.DataSource;
@@ -22,5 +23,10 @@ public class StartupRunner implements CommandLineRunner {
 
     public void run(String... args) throws Exception {
         logger.info("Number of books: " + bookRepository.count());
+    }
+
+    @Scheduled(initialDelay = 1000, fixedRate = 10000)
+    public void run() {
+        logger.info("Number	of	books:	" + bookRepository.count());
     }
 }
