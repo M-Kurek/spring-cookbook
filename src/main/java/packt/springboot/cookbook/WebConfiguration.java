125ec9259a20d8a451d306443808dbc67fc5e0ba
M-Kurek <kurekmaciek@gmail.com>
2017-10-06 20:22:08 +0200

    up to custom interceptors

--- src/main/java/packt/springboot/cookbook/WebConfiguration.java
+++ src/main/java/packt/springboot/cookbook/WebConfiguration.java
@@ -3,16 +3,34 @@ package packt.springboot.cookbook;
 import org.apache.catalina.filters.RemoteIpFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
 @Configuration
-public class WebConfiguration {
+public class WebConfiguration extends WebMvcConfigurerAdapter {
     /**
      * Out	of	the	box,	Spring	Boot	automatically	adds OrderedCharacterEncodingFilter	and	HiddenHttpMethodFilter
      * this one is convenient Tomcat 8 filter to translate IP behind the load balancer proxy
+     *
      * @return
      */
     @Bean
     public RemoteIpFilter remoteIpFilter() {
         return new RemoteIpFilter();
+    }
+
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+    /**
+     * together with addInterceptors (came from WebMvcConfigurerAdapter)
+     * @return
+     */
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        return new LocaleChangeInterceptor();
     }
 }
